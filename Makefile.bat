fb3bf8cb9933473229c0a7b6a4456a1ee87667ee
sadeqsoli <solisadeq@gmail.com>
2025-02-14 16:03:33 +0330

    debugging the code after the issue with the yellow color.

--- Makefile.bat
+++
@@ -1,3 +0,0 @@
-all:
-	dasm colorbg/*.asm -f3 -v0 -obin/colorCart.bin
-
--- colorbg/colorbg.asm
+++ colorbg/colorbg.asm
@@ -12,13 +12,14 @@
 START:
 	CLEAN_START    ; Call macro to safely clear the memory
 
+AfterCleanMem:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Set background luminance color to yellow (NTSC color code $1E)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     lda #$1E       ; Load color code into A register
     sta COLUBK     ; Store A to memory address $09 (TIA COLUBK)
 
-    jmp START      ; Repeat from START
+    jmp AfterCleanMem      ; Repeat from START
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Fill ROM size to exactly 4KB
